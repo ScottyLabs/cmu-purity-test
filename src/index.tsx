f4234a616882da237072e99919d43821d62b640a
Anish Pallati <anishp@duck.com>
2025-03-11 13:31:15 -0400

    chore: add tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,26 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 import type { FC } from "hono/jsx";
 
 const app = new Hono()
+app.use("/static/*", serveStatic({ root: "./" }));
 
 const Layout: FC = (props) => {
   return (
     <html>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>CMU Purity Test</title>
+
+        <link href="/static/tailwind.css" rel="stylesheet" type="text/css" />
+      </head>
       <body>{props.children}</body>
     </html>
   )
 }
 
-const Home: FC = (props) => {
+const Home: FC = () => {
   return (
     <Layout>
       <p>Hello world</p>
