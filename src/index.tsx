58e94540f842e88dd56e9fcb45697765ba75e364
Anish Pallati <anishp@duck.com>
2025-03-11 13:43:25 -0400

    chore: add formatting and conventional commits

--- src/index.tsx
+++ src/index.tsx
@@ -2,34 +2,41 @@ import { Hono } from "hono";
 import { serveStatic } from "hono/bun";
 import type { FC } from "hono/jsx";
 
-const app = new Hono()
+const app = new Hono();
 app.use("/static/*", serveStatic({ root: "./" }));
 
 const Layout: FC = (props) => {
-  return (
-    <html>
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>CMU Purity Test</title>
+	return (
+		<html lang="en">
+			<head>
+				<meta charset="UTF-8" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				/>
+				<title>CMU Purity Test</title>
 
-        <link href="/static/tailwind.css" rel="stylesheet" type="text/css" />
-      </head>
-      <body>{props.children}</body>
-    </html>
-  )
-}
+				<link
+					href="/static/tailwind.css"
+					rel="stylesheet"
+					type="text/css"
+				/>
+			</head>
+			<body>{props.children}</body>
+		</html>
+	);
+};
 
 const Home: FC = () => {
-  return (
-    <Layout>
-      <p>Hello world</p>
-    </Layout>
-  )
-}
+	return (
+		<Layout>
+			<p>Hello world</p>
+		</Layout>
+	);
+};
 
-app.get('/', (c) => {
-  return c.html(<Home />)
-})
+app.get("/", (c) => {
+	return c.html(<Home />);
+});
 
-export default app
+export default app;
